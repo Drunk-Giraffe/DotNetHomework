4bd5d90e2502593d560561f46548edec240f60a5
1041429505@qq.com <1041429505@qq.com>
2021-03-04 17:34:25 +0800

    1.1

---
+++ Exercise1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个运算数：");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个运算数：");
+            double y = Double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入运算符：");
+            char c = Console.ReadLine()[0];
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine("运算结果为："+ (x + y)); 
+                    break;
+                case '-':
+                    Console.WriteLine("运算结果为：" + (x - y));
+                    break;
+                case '*':
+                    Console.WriteLine("运算结果为：" + (x * y));
+                    break;
+                case '/':
+                    if (y != 0)
+                        Console.WriteLine("运算结果为：" + (x / y));
+                    else
+                        Console.WriteLine( "除数不能为0，无法计算！");
+                    break;
+                default:
+                    Console.WriteLine("运算符不合法！");
+                    break;
+            }
+        }
+    }
+}
