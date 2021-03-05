452f8fb8cce27fe5b119a43b1b8441f81286092b
1041429505@qq.com <1041429505@qq.com>
2021-03-05 09:27:55 +0800

    2.1

---
+++ Exercise2/2.1/2.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("请输入数字：");
+            int num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(num+"的质因数有：");
+            p.FindPrimeFactors(num);
+        }
+
+        bool IsPrimeNumber(int num)
+        {
+            bool flag=true;
+            if (num == 2||num==3)
+                return true;
+            for (int i = 2; i < Math.Sqrt(num) + 1; i++)
+                if (num %i == 0)
+                    flag=false;
+            return flag;
+        }
+        void FindPrimeFactors(int num)
+        {
+            int i = 2;
+            if (IsPrimeNumber(num))
+                Console.WriteLine(num);
+            while(!IsPrimeNumber(num))
+            {
+                if (num % i == 0&&IsPrimeNumber(i))
+                {
+                    Console.Write(i + " ");
+                    num = num / i;
+                }
+                else
+                    i++;
+            }
+            Console.Write(num);
+        }
+    }
+}
