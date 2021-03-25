2c5c2d4b80a268cfe633cedd283524b4bb8689c6
1041429505@qq.com <1041429505@qq.com>
2021-03-25 16:33:35 +0800

    4.2

---
+++ Exercise4/4.2/4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clock Clock = new clock();
+            Clock.Tick += s => Console.WriteLine("Tick,Tock");
+            Clock.Alarm+= s => Console.WriteLine("Ringing!当前时间为"+Clock.hour.ToString()+"时" + Clock.minute.ToString() + "分" + Clock.second.ToString() + "秒");
+            Clock.ClockOn();
+        }
+    }
+}
---
+++ Exercise4/4.2/4.2/clock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _4._2
+{
+    class clock
+    {
+        static int passTime = 0;
+        public delegate void ActionHandler(object sender);
+        public event ActionHandler Tick;
+        public event ActionHandler Alarm;
+        private bool clockOn = false;
+        public int hour { get;set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+        public void TimeGoesBy()
+        {
+            second++;
+            if (second == 60)
+            {
+                second = 0;
+                minute++;
+            }
+            if (minute == 60)
+            {
+                minute = 0;
+                hour++;
+            }
+            if (hour == 24)
+            {
+                hour = 0;
+            }
+        }
+        public void ClockOn()
+        {
+            clockOn = true;
+            hour= DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
+            while (clockOn)
+            {
+                Tick(this);
+                passTime++;
+                if (passTime == 10)
+                {
+                    Alarm(this);
+                    passTime = 0;
+                }
+                Thread.Sleep(1000);
+                TimeGoesBy();
+            }
+        }
+    }
+}
